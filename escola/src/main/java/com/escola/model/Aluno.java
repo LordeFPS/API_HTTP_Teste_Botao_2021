c19786693794b164c87e41be2e923883f614ab65
lordefps <lodizinho_mice@hotmail.com>
2021-01-24 20:22:41 -0300

    Entidade

--- escola/src/main/java/com/escola/model/Aluno.java
+++ escola/src/main/java/com/escola/model/Aluno.java
@@ -1,7 +1,15 @@
 package com.escola.model;
 
-public class Aluno {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Aluno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nomeAluno;
 	private String cpf;
